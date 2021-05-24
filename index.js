c6a24c8532114dec06f2b4e9a9babf3d6091b2f3
julianxlozano <julianxlozano@gmail.com>
2021-05-24 13:26:15 -0500

    commit tree 5381d25b5f4a24eb7d5ae4f9f8de5d179d2c01a1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names,eventName){
+    let newNames = [];
+    for (let i=0; i<names.length;i++){
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    };
+    return newNames
+};
+
+function countDown(number){
+   let count = number
+   while (count > -1){
+        console.log(count)
+        count--
+   }
+};
+
